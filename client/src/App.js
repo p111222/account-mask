c744ecc5b02564e2d94ffa78bcefb93719e23356
Adithya S Nair <adithyasnair00@gmail.com>
2023-12-14 17:04:10 +0530

    app fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
-<<<<<<< Updated upstream
-import './App.css';
-=======
 import React from 'react'
->>>>>>> Stashed changes
-
 
 const App = () => {
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      
-=======
     <div>
-      App
->>>>>>> Stashed changes
+      
     </div>
   )
 }
 
 export default App
-
-
-
