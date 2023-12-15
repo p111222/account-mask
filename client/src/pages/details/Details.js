de95d8c1fa056c6ff408000702af83ca95b14411
p111222 <princejainpre.ram@gmail.com>
2023-12-15 14:52:42 +0530

    logout added

--- client/src/pages/details/Details.js
+++ client/src/pages/details/Details.js
@@ -3,13 +3,26 @@ import AllCorporateDetails from '../../components/AllCorporateDetails'
 import CorporateUserDetails from '../../components/CorporateUserDetails'
 import { useContext } from 'react';
 import { AuthContext } from '../../context/authContext';
-
+import { makeRequest } from '../../axios';
+import { useNavigate } from 'react-router-dom';
 
 const Details = () => {
     const { currentUser } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        makeRequest.get('/logout')
+            .then((res) => {
+                if(res.status===200){
+                    localStorage.clear();
+                    navigate("/login")
+                }
+             })
+    }
 
     return (
         <>
+            <button onClick={handleLogout} className='btn btn-danger me-3 mt-2 position-fixed' style={{ right: '0' }}>Logout</button>
             {currentUser.usertype == 'bank' ?
                 <AllCorporateDetails /> :
                 <CorporateUserDetails />}
